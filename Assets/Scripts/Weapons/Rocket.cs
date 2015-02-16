ddcada3de8088b058617bb7da2ace9c4bc349b72
Riku Erkkilä <riku.erkkila@gmail.com>
2015-02-17 00:25:58 +0200

    Mortar

--- Assets/Scripts/Weapons/Rocket.cs
+++ Assets/Scripts/Weapons/Rocket.cs
@@ -4,43 +4,48 @@ using System.Collections;
 public class Rocket : MonoBehaviour 
 {
 	public GameObject explosion;		// Prefab of explosion effect.
-    [HideInInspector]
-    public PlayerControl player;
-
+	
+	
 	void Start () 
 	{
-		// Destroy the rocket after 2 seconds if it doesn't get destroyed before then.
-		Destroy(gameObject, 2);
+		// Destroy the rocket after 4 seconds if it doesn't get destroyed before then.
+		Destroy(gameObject, 4);
 	}
-
-
+	
+	void Update ()
+	{
+		Vector2 velocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+		float angle = Mathf.Atan2( velocity.y, velocity.x );
+		transform.eulerAngles = new Vector3(0, 0, angle *  Mathf.Rad2Deg);
+	}
+	
 	void OnExplode()
 	{
 		// Create a quaternion with a random rotation in the z-axis.
 		Quaternion randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
-
+		
 		// Instantiate the explosion where the rocket is with the random rotation.
 		Instantiate(explosion, transform.position, randomRotation);
 	}
 	
 	void OnTriggerEnter2D (Collider2D col) 
 	{
-
+		
 		// If the hit object is damageable...
 		if (col.gameObject.GetComponent<Damageable>() != null) {
 			// ...give it 10 hitpointd of damage
 			col.gameObject.GetComponent<Damageable>().TakeDamage(10);
 		}
-
+		
 		// If it hits an enemy...
 		if(col.tag == "Enemy")
 		{
 			// ... find the Enemy script and call the Hurt function.
 			col.gameObject.GetComponent<Enemy>().Hurt();
-
+			
 			// Call the explosion instantiation.
 			OnExplode();
-
+			
 			// Destroy the rocket.
 			Destroy (gameObject);
 		}
@@ -49,28 +54,27 @@ public class Rocket : MonoBehaviour
 		{
 			// ... find the Bomb script and call the Explode function.
 			col.gameObject.GetComponent<Bomb>().Explode();
-
+			
 			// Destroy the bomb crate.
 			Destroy (col.transform.root.gameObject);
-
+			
 			// Destroy the rocket.
 			Destroy (gameObject);
 		}
 		// Otherwise if the player manages to shoot himself...
 		else if(col.gameObject.tag == "Player")
 		{
-
 			// Instantiate the explosion and destroy the rocket.
 			OnExplode();
-            PlayerHealth pH = col.gameObject.GetComponent<PlayerHealth>();
-            pH.TakeDamage(gameObject.transform, player);
-            Destroy (gameObject);
-
+			PlayerHealth pH = col.gameObject.GetComponent<PlayerHealth>();
+			pH.TakeDamage(gameObject.transform, null);
+			Destroy (gameObject);
+			
 		}
-        else if (col.gameObject.tag != "Player")
-        {
-            OnExplode();
-            Destroy(gameObject);
-        }
+		else if (col.gameObject.tag != "Player")
+		{
+			OnExplode();
+			Destroy(gameObject);
+		}
 	}
 }
