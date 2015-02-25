d09631d6a22dc4b1d2992cc36b11bbaf248d54d0
Riku Erkkilä <riku.erkkila@gmail.com>
2015-02-25 17:10:06 +0200

    Damageable = abstract base class for damageable objects

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -3,51 +3,29 @@ using System.Collections;
 
 
 
-public class Damageable : MonoBehaviour {
+abstract public class Damageable : MonoBehaviour {
 
 	public int maxHitpoints = 10;				// hitpoints when undamaged
-	public int hitpointsLightDamage = 8;		// hitpoints when lightly damaged sprite is applied 
-	public int hitpointsHeavyDamage = 4;		// hitpoints when heavily damaged sprite is applied
-
-	public Sprite spriteLightDamage = null;		// sprite for lightly damaged object
-	public Sprite spriteHeavyDamage = null;		// sprite for heavily damaged object
-
-	public GameObject leftWhenDestroyed = null;  // When hitpoints reach zero, the gameObject is replaced with this one
-
-	private int hitpointsLeft = 10;
+	protected int currentHitpoints = 10;
 
 
 	void Awake () {
-		hitpointsLeft = maxHitpoints;
+		currentHitpoints = maxHitpoints;
 	}
 
-	public void TakeDamage( int hitpoints ) {
-
-		hitpointsLeft -= hitpoints;
+	public virtual void TakeDamage( int hitpoints ) {
+		currentHitpoints -= hitpoints;
 
 		// If there is no hitpoints left, destroy the object.
-		if (hitpointsLeft <= 0) {
+		if (currentHitpoints <= 0) {
 			OnDestroyed();
 			return;
 		}
 
-		SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
-
-		// Change sprite according to hitpoints left
-		if (hitpointsLeft <= hitpointsHeavyDamage && spriteHeavyDamage != null)
-			sr.sprite = spriteHeavyDamage;
-		else if (hitpointsLeft <= hitpointsLightDamage && spriteLightDamage != null)
-			sr.sprite = spriteLightDamage;
-
 	}
 
-	private void OnDestroyed() {
-
-		if (leftWhenDestroyed != null) {
-			GameObject ruin = Instantiate(leftWhenDestroyed, transform.position, transform.rotation) as GameObject;
-			ruin.transform.localScale = transform.localScale;
-
-		}
+	protected virtual void OnDestroyed() {
+		Debug.Log("D3");
 
 		Destroy(gameObject);
 	}
---
+++ Assets/Scripts/DamageableBlock.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class DamageableBlock : Damageable {
+
+//	public int maxHitpoints = 10;				// hitpoints when undamaged
+	public int hitpointsLightDamage = 8;		// hitpoints when lightly damaged sprite is applied 
+	public int hitpointsHeavyDamage = 4;		// hitpoints when heavily damaged sprite is applied
+
+	public Sprite spriteLightDamage = null;		// sprite for lightly damaged object
+	public Sprite spriteHeavyDamage = null;		// sprite for heavily damaged object
+
+	public GameObject leftWhenDestroyed = null;  // When hitpoints reach zero, the gameObject is replaced with this one
+
+//	private int currentHitpoints = 10;
+
+
+//	void Awake () {
+//		hitpointsLeft = maxHitpoints;
+//	}
+
+	public override void TakeDamage( int hitpoints ) {
+
+		base.TakeDamage( hitpoints ); // also destroys object on 0 hp
+
+		SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+
+		// Change sprite according to hitpoints left
+		if (currentHitpoints <= hitpointsHeavyDamage && spriteHeavyDamage != null)
+			sr.sprite = spriteHeavyDamage;
+		else if (currentHitpoints <= hitpointsLightDamage && spriteLightDamage != null)
+			sr.sprite = spriteLightDamage;
+
+		if (currentHitpoints <= 0 && leftWhenDestroyed != null) {
+			LeaveRuins();
+		}
+
+		
+		
+	}
+
+	private void LeaveRuins() {
+
+			GameObject ruin = Instantiate(leftWhenDestroyed, transform.position, transform.rotation) as GameObject;
+			ruin.transform.localScale = transform.localScale;
+
+	}
+
+
+
+	            
+}
