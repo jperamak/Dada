4076458ed60f72afbe5d308b06c98e2ca6f8d27a
Jussi Perämäki <jussi.peramaki@gmail.com>
2015-02-26 01:23:51 +0200

    new explosion logic, not final

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	// Use this for initialization
+	public void Explode (PlayerControl source, float radius) {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, radius, LayerMask.GetMask(new string[] { "Enemy", "Player", "Ground", "BackgroundBlock" }));
+        foreach (Collider2D col in hits)
+        {
+            // If the hit object is damageable...
+            if (col.gameObject.GetComponent<Damageable>() != null)
+            {
+                // ...give it 10 hitpointd of damage
+                col.gameObject.GetComponent<Damageable>().TakeDamage(10);
+            }
+
+            // Otherwise if the player manages to shoot himself...
+            else if (col.gameObject.tag == "Player")
+            {
+
+                // Instantiate the explosion and destroy the rocket.
+                PlayerHealth pH = col.gameObject.GetComponent<PlayerHealth>();
+                pH.TakeDamage(gameObject.transform, source);
+                //OnExplode();
+            }
+        }
+	}
+
+
+    public void BetterExplode(PlayerControl source, float radius)
+    {
+        Vector2 dir;
+        for (int i = 1; i < 9; i++)
+        {
+            dir = Quaternion.AngleAxis(i*45f,Vector3.forward)*Vector2.up;
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, radius, LayerMask.GetMask(new string[] { "Enemy", "Player", "Ground", "BackgroundBlock" }));
+            if (hit)
+            {
+                if (hit.rigidbody)
+                    //Debug.Log(hit.transform.gameObject);
+                    hit.rigidbody.AddForce(dir*1000);
+                // If the hit object is damageable...
+                if (hit.collider.gameObject.GetComponent<Damageable>() != null)
+                {
+                    // ...give it 10 hitpointd of damage
+                    hit.collider.gameObject.GetComponent<Damageable>().TakeDamage(5);
+                }
+
+                // Otherwise if the player manages to shoot himself...
+                else if (hit.collider.gameObject.tag == "Player")
+                {
+
+                    // Instantiate the explosion and destroy the rocket.
+                    PlayerHealth pH = hit.collider.gameObject.GetComponent<PlayerHealth>();
+                    pH.TakeDamage(gameObject.transform, source);
+                    //OnExplode();
+                }
+            }
+        }
+    }
+
+}
--- Assets/Scripts/Weapons/Rocket.cs
+++ Assets/Scripts/Weapons/Rocket.cs
@@ -11,7 +11,8 @@ public class Rocket : MonoBehaviour
 	void Start () 
 	{
 		// Destroy the rocket after 4 seconds if it doesn't get destroyed before then.
-		Destroy(gameObject, 4);
+        Invoke("OnExplode", 4);
+        //Destroy(gameObject, 4);
 	}
 	
 	void Update ()
@@ -27,39 +28,13 @@ public class Rocket : MonoBehaviour
 		Quaternion randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
 		
 		// Instantiate the explosion where the rocket is with the random rotation.
-		Instantiate(explosion, transform.position, randomRotation);
+        GameObject expl = (GameObject)Instantiate(explosion, transform.position, randomRotation);
+        expl.GetComponent<Explosion>().BetterExplode(player, radius);
         Destroy(gameObject);
 	}
 	
 	void OnTriggerEnter2D (Collider2D c) 
 	{
-        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, radius, LayerMask.GetMask(new string[] { "Enemy", "Player", "Ground", "BackgroundBlock" }));
-        foreach (Collider2D col in hits)
-        {
-            //Debug.Log(c);
-            // If the hit object is damageable...
-            if (col.gameObject.GetComponent<Damageable>() != null)
-            {
-                // ...give it 10 hitpointd of damage
-                col.gameObject.GetComponent<Damageable>().TakeDamage(10);
-            }
-
-
-
-            // Otherwise if the player manages to shoot himself...
-            else if (col.gameObject.tag == "Player")
-            {
-
-                // Instantiate the explosion and destroy the rocket.
-                PlayerHealth pH = col.gameObject.GetComponent<PlayerHealth>();
-                pH.TakeDamage(gameObject.transform, player);
-                //OnExplode();
-            }
-            else if (col.gameObject.tag != "Player")
-            {
-                //OnExplode();
-            }
-        }
         OnExplode();
     }
 	
