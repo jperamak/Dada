dd7cda8a4014f5b8cf4865bbcea9e132172a513b
Jussi Perämäki <jussi.peramaki@gmail.com>
2015-03-01 00:39:50 +0200

    fixed silly camera bug

--- Assets/Scripts/Remover.cs
+++ Assets/Scripts/Remover.cs
@@ -3,11 +3,17 @@ using System.Collections;
 
 public class Remover : MonoBehaviour
 {
-	void OnTriggerEnter2D(Collider2D col)
-	{
-		// If the player hits the trigger...
-
-			Destroy (col.gameObject);	
-
-	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // If the player hits the trigger...
+        if (col.gameObject.tag == "Player")
+        {
+            PlayerControl pc = col.gameObject.GetComponent<PlayerControl>();
+            pc.Die();
+        }
+        else
+        {
+            Destroy(col.gameObject);
+        }
+    }
 }
