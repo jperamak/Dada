42382036cc1233dc73b39d4124911cf27628c920
Riku Erkkilä <riku.erkkila@gmail.com>
2015-03-04 00:15:37 +0200

    Explosion dampened by fg walls, new sprites to building blocks (again)

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -9,7 +9,7 @@ public class Explosion : MonoBehaviour {
         Vector2 position = new Vector2(transform.position.x, transform.position.y);
 
         //float divider = 4 * radius > 4 ? 4 * radius : 4;
-		float divider = 64f; 
+		float divider = 15f; 
 		float invDiv = 1/divider;
         float step = 360f * invDiv;
 
@@ -17,6 +17,8 @@ public class Explosion : MonoBehaviour {
         {
             dir = Quaternion.AngleAxis(step*0.5f + step*i,Vector3.forward)*Vector2.up;
             RaycastHit2D[] hits = Physics2D.LinecastAll(position, position + dir * radius, LayerMask.GetMask(new string[] { "Enemy", "Player", "Ground", "BackgroundBlock", "Rubble" }));
+
+			int numTimesDamped = 0; // How many times the ray has hit a object that dampens the explosion
 
 			foreach (RaycastHit2D hit in hits)
             {
@@ -26,21 +28,25 @@ public class Explosion : MonoBehaviour {
                 // If the hit object is damageable...
                 if (hit.collider.gameObject.GetComponent<Damageable>() != null)
                 {
-                    // ...give it the damage
-                    hit.collider.gameObject.GetComponent<Damageable>().TakeDamage((float)damage * invDiv);
+					// calculate the basic damage
+					float rayDamage = (float)damage * invDiv;
+					// apply dampening
+					if (numTimesDamped > 0)
+						rayDamage = rayDamage * Mathf.Pow(0.75f, numTimesDamped);
+                    // inflict the damage
+                    hit.collider.gameObject.GetComponent<Damageable>().TakeDamage(rayDamage);
                 }
 
-                // Otherwise if the player manages to shoot himself...
-                else if (hit.collider.gameObject.tag == "Player")
-				{	Debug.Log("Player hit by explosion");
-
+                // If a player is hit before the explosion is dampened... 
+                if (hit.collider.gameObject.tag == "Player" && numTimesDamped == 0)
+				{	
                     // Instantiate the explosion and destroy the rocket.
                     PlayerHealth pH = hit.collider.gameObject.GetComponent<PlayerHealth>();
                     pH.TakeDamage(source);
                 }
-                // dont hit things behind solid blocks
+                // Solid objects in foreground dampen the explosion
 				if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Ground"))
-                    break;
+                    numTimesDamped++;
             }
         }
     }
