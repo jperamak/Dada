7622462b05bbc9e9b918a7f7c7bd1a1e9f6180a0
Riku Erkkilä <riku.erkkila@gmail.com>
2015-03-09 16:11:37 +0200

    Ridiculously overpowered TNT-barrels

---
+++ Assets/Scripts/Weapons/GenericExplosion.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericExplosion : MonoBehaviour {
+
+	public GameObject Explosion;		// Prefab of explosion effect.
+	public float Radius;
+	public float ExplosionForce;
+	public int Damage;
+	public PlayerControl Player;
+	
+	void Awake () {
+		Quaternion randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+		
+		// Instantiate the explosion with random rotation.
+		GameObject expl = (GameObject)Instantiate(Explosion, transform.position, randomRotation);
+		expl.GetComponent<Explosion>().Explode(Player, Radius, ExplosionForce, Damage);
+		Destroy(gameObject);
+	}
+}
