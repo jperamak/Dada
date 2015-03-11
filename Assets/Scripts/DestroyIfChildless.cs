8baf77e681f283a92fbf6610a014416ee8a64a52
Riku Erkkilä <riku.erkkila@gmail.com>
2015-03-11 21:48:00 +0200

    Croc fixes

---
+++ Assets/Scripts/DestroyIfChildless.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyIfChildless : MonoBehaviour {
+
+	// could also be checked less often...
+	void Update () 
+	{
+		if (transform.childCount == 0)
+			Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/FlipIfUpsideDown.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipIfUpsideDown : MonoBehaviour {
+	
+	public float MaxUpsideDown = 2.5f;
+	public float FlipForce = 2.0f;
+
+	private float _timeUpsideDown = 0;
+
+		
+	
+	void Update(){
+		
+		float zAngle = transform.rotation.eulerAngles.z;
+		if(zAngle > 100 && zAngle < 260)
+			_timeUpsideDown += Time.deltaTime;
+		else if(zAngle < 10)
+			_timeUpsideDown = 0;
+			
+		if(_timeUpsideDown > MaxUpsideDown){
+			Flip();
+			_timeUpsideDown = 0;
+		}
+
+	}
+	
+	
+	void Flip(){
+		float multiplier = Mathf.Clamp((int)(_timeUpsideDown/MaxUpsideDown) * 20.0f * transform.localScale.x,0,100.0f);
+		
+		transform.GetComponent<Rigidbody2D>().AddForce(-transform.up * (FlipForce+multiplier), ForceMode2D.Impulse);
+	}
+}
