dbd8db4b9092e2855eb96859a5bf7f7272aaa81d
Riku Erkkilä <riku.erkkila@gmail.com>
2015-03-19 02:57:26 +0200

    Glue. Floors and support beams

---
+++ Assets/Scripts/LevelsCommon/Glue.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// A helper component that attaches objects with RigidBody2D together by creating 
+// a non-rotating hinge.
+
+public class Glue : MonoBehaviour {
+
+	public GameObject[] gluedObjects;
+
+	void Start () {
+
+		foreach ( GameObject gobj in gluedObjects) {
+			if (gobj != null) 
+				CreateHingeWith(gobj);
+		}
+	}
+
+	void CreateHingeWith(GameObject gobj) {
+		HingeJoint2D hj = gameObject.AddComponent<HingeJoint2D>();
+		
+		// Connect hinge to the other (glued) object
+		if(gobj.GetComponent<Rigidbody2D>() != null)
+			hj.connectedBody = gobj.GetComponent<Rigidbody2D>();
+		else
+			Debug.LogError("Tried to glue a GameObject without RigidBody2D");
+		
+		// Limit the joint so it cannot rotate
+		hj.useLimits = true;
+		hj.limits = new JointAngleLimits2D();
+		
+		// Set anchor point to other objects center
+		hj.anchor = transform.InverseTransformPoint( gobj.transform.position );
+	}
+
+
+
+}
