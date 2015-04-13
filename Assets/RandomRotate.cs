46868698774dc5f9e970f6e8882a22d9c77fce79
Jussi Perämäki <jussi.peramaki@gmail.com>
2015-04-14 00:22:00 +0300

    tweaks

---
+++ Assets/RandomRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.rotation = transform.rotation * Quaternion.Euler(new Vector3(0, 0, 90 * Random.RandomRange(0, 3)));
+        Debug.Log(transform.rotation);
+	}
+
+}
