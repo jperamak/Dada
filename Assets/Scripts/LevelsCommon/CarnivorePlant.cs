8ad14bb8f61897ec0b7817a4cfc3f508997ae4fe
Riku Erkkilä <riku.erkkila@gmail.com>
2015-05-13 23:57:43 +0300

    Carnivorous plant

---
+++ Assets/Scripts/LevelsCommon/CarnivorePlant.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+// I was drunk as shit when I coded this so be bear with with me.
+public class CarnivorePlant : MonoBehaviour {
+
+	public float detectHeroRange;
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+		Vector2 preyVector = GetVectorToClosestHero();
+
+		if (preyVector.magnitude < detectHeroRange) {
+			preyVector.Normalize();
+			preyVector = Vector2.Scale( preyVector, new Vector2(force,force));
+			this.GetComponent<Rigidbody2D>().AddForce(preyVector,ForceMode2D.Force);
+		}
+
+	}
+
+	Vector2 GetVectorToClosestHero() {
+
+		// TODO: Ineffective, fix
+		Object[] heros = FindObjectsOfType(typeof(Hero));
+		
+		Hero closestHero = null;
+		
+		for (int i=0; i<heros.Length; i++) {
+			Hero hero = (Hero)heros[i];
+
+			if (closestHero == null)
+				closestHero = hero;
+			else if (RangeToHero(hero) < RangeToHero(closestHero))
+				closestHero = hero;
+		}
+
+		if (closestHero == null)
+			return new Vector2(0f,0f);
+
+		return closestHero.transform.position - transform.position;
+
+	}
+
+	float RangeToHero(Hero hero) {
+		return Vector2.Distance( hero.transform.position, transform.position );
+	}
+
+}
