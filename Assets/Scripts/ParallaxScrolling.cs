35c6ac9b03b24e6aa5d936f1da7138425915a2c0
Riku Erkkilä <riku.erkkila@gmail.com>
2015-05-17 23:16:39 +0300

    Background implemented

---
+++ Assets/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScrolling : MonoBehaviour 
+{
+
+	public Transform Maincamera;
+	public float scrollFactor = 0.5f;
+	public float autoScrollXSpeed = 0.0f;
+		
+		
+		// Update is called once per frame
+		//public float scrollSpeed = 0.5F;
+		void Update() 
+		{
+			//	float offset = Time.time * scrollSpeed;
+			Vector2 texOffset = new Vector2( Maincamera.position.x * scrollFactor / transform.localScale.x + autoScrollXSpeed * Time.time, 
+		                                     Maincamera.position.y * scrollFactor / transform.localScale.y);
+			GetComponent<Renderer>().material.SetTextureOffset("_MainTex", texOffset);
+		}
+	}
