97042d7712425e014ba05190ab157f9f38e4913c
Jussi Perämäki <jussi.peramaki@gmail.com>
2015-05-23 19:20:38 +0300

    zoomstuff

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,24 +8,32 @@ public class CameraFollow : MonoBehaviour
 	public float yMargin = 1f;		// Distance in the y axis the player can move before the camera follows.
 	public float xSmooth = 8f;		// How smoothly the camera catches up with it's target movement in the x axis.
 	public float ySmooth = 8f;		// How smoothly the camera catches up with it's target movement in the y axis.
-	public Vector2 maxXAndY;		// The maximum x and y coordinates the camera can have.
+    public float zoomSmooth = 2f;
+
+    public Vector2 maxXAndY;		// The maximum x and y coordinates the camera can have.
 	public Vector2 minXAndY;		// The minimum x and y coordinates the camera can have.
     public float maxZoom;
     public float minZoom;
-
+    public float zoomMod = 0.6f;
 
     public Vector2 average;
     private Vector2 minPlayer;
     private Vector2 maxPlayer;
     private float vertExtent;
     private float horExtent;
+    private Vector3 topRight, bottomLeft;
+
+
 
 	public List<Transform> players;		// Reference to the player's transform.
 
-
 	void Awake ()
 	{
-		// Setting up the reference.
+
+        topRight = GameObject.Find("cameraTopRightBound").transform.position;
+        bottomLeft = GameObject.Find("cameraBottomLeftBound").transform.position;
+        
+        // Setting up the reference.
         average = Vector2.zero;
         minPlayer = Vector2.zero;
         maxPlayer = Vector2.zero;
@@ -48,6 +56,14 @@ public class CameraFollow : MonoBehaviour
         foreach (Transform t in players)
             if (Mathf.Abs(transform.position.x - t.position.x) > xMargin)
                 return true;
+         if ( transform.position.x - xMargin  < bottomLeft.x)
+        {
+            minPlayer.x = bottomLeft.x;
+        }
+        if ( transform.position.x + xMargin  > topRight.x)
+        {
+            maxPlayer.x = topRight.x;
+        }
         return false;
 	}
 
@@ -57,7 +73,17 @@ public class CameraFollow : MonoBehaviour
 		// Returns true if the distance between the camera and the player in the y axis is greater than the y margin.
         foreach (Transform t in players)
             if (Mathf.Abs(transform.position.y - t.position.y) > yMargin)
+            {
                 return true;
+            }
+        if ( transform.position.y - yMargin  < bottomLeft.y)
+        {
+            minPlayer.y = bottomLeft.y;
+        }
+        if ( transform.position.y + yMargin  > topRight.y)
+        {
+            maxPlayer.y = topRight.y;
+        }
         return false;
 	}
 
@@ -70,20 +96,20 @@ public class CameraFollow : MonoBehaviour
 	
     void AdjustZoom()
     {
-        float x = Mathf.Abs(minPlayer.x - maxPlayer.x);
-        float y = Mathf.Abs(minPlayer.y - maxPlayer.y);
+        float x = (maxPlayer.x - minPlayer.x) * zoomMod;
+        float y = (maxPlayer.y - minPlayer.y) * zoomMod;
 
 
         if (x < minZoom && y < minZoom)
-            GetComponent<Camera>().orthographicSize = minZoom;
-        else //if (x > camera.orthographicSize || y > camera.orthographicSize)
+            GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, minZoom, zoomSmooth * Time.deltaTime);
+        else
         {
-            GetComponent<Camera>().orthographicSize = x > y ? x : y;
-            GetComponent<Camera>().orthographicSize = GetComponent<Camera>().orthographicSize < maxZoom ? GetComponent<Camera>().orthographicSize : maxZoom;
+            float zoom = x > y ? x : y;
+            zoom = Mathf.Lerp(GetComponent<Camera>().orthographicSize, zoom, zoomSmooth * Time.deltaTime);
+            GetComponent<Camera>().orthographicSize = zoom < maxZoom ? zoom : maxZoom;
         }
         vertExtent = Camera.main.orthographicSize;
         horExtent = Camera.main.orthographicSize * Screen.width / Screen.height;
-
     }
 	
 	void TrackPlayer ()
@@ -101,13 +127,10 @@ public class CameraFollow : MonoBehaviour
             average = (minPlayer + maxPlayer) * 0.5f;
         }
 
-
-
 		// By default the target x and y coordinates of the camera are it's current x and y coordinates.
 		float targetX = transform.position.x;
 		float targetY = transform.position.y;
 
-        AdjustZoom();
 
 		// If the player has moved beyond the x margin...
 		if(CheckXMargin())
@@ -119,9 +142,12 @@ public class CameraFollow : MonoBehaviour
 			// ... the target y coordinate should be a Lerp between the camera's current y position and the player's current y position.
             targetY = Mathf.Lerp(transform.position.y, average.y, ySmooth * Time.deltaTime);
 
+        AdjustZoom();
+
 		// The target x and y coordinates should not be larger than the maximum or smaller than the minimum.
         targetX = Mathf.Clamp(targetX, horExtent + minXAndY.x * 0.5f , maxXAndY.x * 0.5f - horExtent);
         targetY = Mathf.Clamp(targetY, vertExtent +  minXAndY.y * 0.5f, maxXAndY.y * 0.5f - vertExtent);
+
 
 		// Set the camera's position to the target position with the same z component.
 		transform.position = new Vector3(targetX, targetY, transform.position.z);
