53fd1a328de21554d5004f91387d4a584d8cd4da
Riku Erkkilä <riku.erkkila@gmail.com>
2015-05-25 01:14:04 +0300

    New carnivore plant

--- Assets/Scripts/LevelsCommon/CarnivorePlant.cs
+++ Assets/Scripts/LevelsCommon/CarnivorePlant.cs
@@ -8,22 +8,42 @@ public class CarnivorePlant : MonoBehaviour {
 	public float detectHeroRange;
 	public float force;
 
+
+
+	float nextMunch = 0f;
+
 	// Use this for initialization
 	void Start () {
-	
 	}
-	
+
+
 	// Update is called once per frame
 	void Update () {
 
 	
 		Vector2 preyVector = GetVectorToClosestHero();
 
+		// if player is close...
 		if (preyVector.magnitude < detectHeroRange) {
+
+			// apply force to direction of player
 			preyVector.Normalize();
 			preyVector = Vector2.Scale( preyVector, new Vector2(force,force));
 			this.GetComponent<Rigidbody2D>().AddForce(preyVector,ForceMode2D.Force);
+
+			// and start to open and close mouth
+			if (Time.time > nextMunch) {
+				if (this.GetComponent<SpringJoint2D>().distance > 0.5f) {
+					this.GetComponent<SpringJoint2D>().distance = 0.3f;
+					nextMunch = Time.time + 0.4f;
+				}
+				else {
+					this.GetComponent<SpringJoint2D>().distance = 1.9f;
+					nextMunch = Time.time + 0.4f;
+				}
+			}
 		}
+
 
 	}
 
--- Assets/Scripts/LevelsCommon/SpringyHinge.cs
+++ Assets/Scripts/LevelsCommon/SpringyHinge.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class SpringyHinge : MonoBehaviour {
 
+	public Vector2 location = new Vector2(0f,0f);
+
 	// Use this for initialization
 	void Start () {
 	
