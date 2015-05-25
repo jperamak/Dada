379f062cd3d48f3d1f2765f9f8d0a10ca1e14593
Riku Erkkilä <riku.erkkila@gmail.com>
2015-05-25 20:05:17 +0300

    Version control probs

---
+++ Assets/CrateCowboyHat.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrateCowboyHat : MonoBehaviour {
+
+	public Transform target;
+	public float floatSpeed;
+
+	float awakeTime;
+	Animation hatAnim;
+	bool hatInHead = false;
+
+	// Use this for initialization
+	void Awake () {
+		awakeTime = Time.time;
+		hatAnim = this.GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if (target != null) {
+//			Vector3 targetVec = target.transform.position - transform.position;
+
+			transform.position = Vector2.MoveTowards(transform.position, target.transform.position, floatSpeed*Time.deltaTime);
+		//}
+
+		if (awakeTime > 0.5f && (target.transform.position - transform.position).magnitude < 0.1f) {
+			hatInHead = true;
+		}
+
+
+
+
+	}
+}
--- Assets/Teleporter.cs
+++ Assets/Teleporter.cs
@@ -7,25 +7,52 @@ public class Teleporter : MonoBehaviour {
 	public Direction TeleportIfMovesTo = Direction.Right;
 
 
-    public Transform OtherEnd;
+    public Transform OtherEnd = null;
+	public GameObject TeleportsTo = null;
+
+	Teleporter otherTeleport = null;
+
+	void Start() {
+
+	}
+
 
     void OnTriggerStay2D(Collider2D other)
     {
-        if (OtherEnd != null && other.attachedRigidbody != null) 
+
+		if (TeleportsTo != null && IsObjectMovingRightWay( other.attachedRigidbody )) 
 		{
-			// Teleport works only on rigidBodies...
-			// that move to a given direction. (To prevent teleporting back and forth)
-			if ( (TeleportIfMovesTo == Direction.Right && other.attachedRigidbody.velocity.x > 0f) ||
-			     (TeleportIfMovesTo == Direction.Left && other.attachedRigidbody.velocity.x < 0f) )
-			{
-				other.gameObject.transform.position = new Vector3( OtherEnd.position.x, other.gameObject.transform.position.y, other.gameObject.transform.position.z );
-			}
-			if ( (TeleportIfMovesTo == Direction.Up && other.attachedRigidbody.velocity.y > 0f) ||
-			     (TeleportIfMovesTo == Direction.Down && other.attachedRigidbody.velocity.y < 0f) )
-			{
-				other.gameObject.transform.position = new Vector3( other.gameObject.transform.position.x, OtherEnd.position.y, other.gameObject.transform.position.z );
-			}
+			Teleporter otherTeleport = TeleportsTo.GetComponent<Teleporter>();
+			if (otherTeleport == null)
+				Debug.LogError("Receiving end is not a Teleporter");
+
+			float distFromAxis = GetDistanceFromCentralAxis( other.attachedRigidbody );
+			other.transform.position = otherTeleport.GetExitPosition( distFromAxis );
 		}
     }
+
+
+	bool IsObjectMovingRightWay( Rigidbody2D rigidBody ) {
+		return ( (TeleportIfMovesTo == Direction.Right && rigidBody.velocity.x > 0f) ||
+		         (TeleportIfMovesTo == Direction.Left  && rigidBody.velocity.x < 0f) ||
+		         (TeleportIfMovesTo == Direction.Up    && rigidBody.velocity.y > 0f) ||
+		         (TeleportIfMovesTo == Direction.Down  && rigidBody.velocity.y < 0f) );
+	}
+
+	float GetDistanceFromCentralAxis( Rigidbody2D rigidBody ) {
+		if (TeleportIfMovesTo == Direction.Right || TeleportIfMovesTo == Direction.Left)
+			return rigidBody.transform.position.y - transform.position.y;
+		else
+			return rigidBody.transform.position.x - transform.position.x;
+	}
+
+	Vector3 GetExitPosition( float distCentralAxis ) {
+		if (TeleportIfMovesTo == Direction.Right || TeleportIfMovesTo == Direction.Left)
+			return new Vector3(transform.position.x, transform.position.y + distCentralAxis, 0f);
+		return new Vector3(transform.position.x + distCentralAxis, transform.position.y, 0f);
+	}
+
+
+
 
 }
