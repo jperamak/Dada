65e106f9f46bb1ce2ace49449321850f9f8277e7
Riku Erkkilä <riku.erkkila@gmail.com>
2015-06-02 12:45:06 +0300

    Manual control option added to camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -13,16 +13,24 @@ public class CameraFollow : MonoBehaviour
     public float minZoom;
     public float maxZoom;
 
+	public bool manualControlEnabled;
+	public float manualZoomSpeed = 1f;
+
     private Vector2 minPlayer;
     private Vector2 maxPlayer;
     private Vector3 topRight, bottomLeft, average;
-    
+	bool followSinglePlayer;
+	private int followedPlayer;
+
     [HideInInspector]
     public List<Transform> players;
 
 
     void Start()
     {
+		followSinglePlayer = false;
+		followedPlayer = 0;
+
         topRight = GameObject.Find("cameraTopRightBound").transform.position;
         bottomLeft = GameObject.Find("cameraBottomLeftBound").transform.position;
         //float x = (topRight.x - bottomLeft.x) / 2f * Screen.height / Screen.width;
@@ -43,10 +51,40 @@ public class CameraFollow : MonoBehaviour
 
     void Update()
     {
+		if (manualControlEnabled)
+			ManualControl();
         FindCorners();
         AdjustZoom();
         Move();
     }
+
+	void ManualControl() 
+	{
+		if (Input.GetKey(KeyCode.X)) 
+		{
+			minZoom -= manualZoomSpeed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.C)) 
+		{
+			minZoom += manualZoomSpeed * Time.deltaTime;
+		}
+		if (Input.GetKeyDown(KeyCode.Z)) 
+		{
+			if (!followSinglePlayer)
+			{
+				followSinglePlayer = true;
+				followedPlayer = 0;
+			}
+			else 
+			{
+				followedPlayer++;
+			}
+			if (followedPlayer >= players.Count)
+				followSinglePlayer = false;
+		}
+
+
+	}
 
     void Move()
     {
@@ -79,27 +117,34 @@ public class CameraFollow : MonoBehaviour
     {
         minPlayer = topRight;
         maxPlayer = bottomLeft;
-        foreach (Transform p in players)
-        {
-            if (p.position.x < minPlayer.x)
-                minPlayer.x = p.position.x;
-            if (p.position.y < minPlayer.y)
-                minPlayer.y = p.position.y;
 
-            if (p.position.x > maxPlayer.x)
-                maxPlayer.x = p.position.x;
-            if (p.position.y > maxPlayer.y)
-                maxPlayer.y = p.position.y;
-
-            if (maxPlayer.x > topRight.x - buffer)
-                maxPlayer.x = topRight.x - buffer;
-            if (maxPlayer.y > topRight.y - buffer)
-                maxPlayer.y = topRight.y - buffer;
-
-            if (minPlayer.x < bottomLeft.x + buffer)
-                minPlayer.x = bottomLeft.x + buffer;
-            if (minPlayer.y < bottomLeft.y + buffer)
-                minPlayer.y = bottomLeft.y + buffer;
-        }
+		if (followSinglePlayer)
+			processPlayer( players[followedPlayer] );
+		else foreach (Transform p in players)
+			processPlayer(p);
     }
+
+	void processPlayer( Transform p )
+	{
+		if (p.position.x < minPlayer.x)
+			minPlayer.x = p.position.x;
+		if (p.position.y < minPlayer.y)
+			minPlayer.y = p.position.y;
+		
+		if (p.position.x > maxPlayer.x)
+			maxPlayer.x = p.position.x;
+		if (p.position.y > maxPlayer.y)
+			maxPlayer.y = p.position.y;
+		
+		if (maxPlayer.x > topRight.x - buffer)
+			maxPlayer.x = topRight.x - buffer;
+		if (maxPlayer.y > topRight.y - buffer)
+			maxPlayer.y = topRight.y - buffer;
+		
+		if (minPlayer.x < bottomLeft.x + buffer)
+			minPlayer.x = bottomLeft.x + buffer;
+		if (minPlayer.y < bottomLeft.y + buffer)
+			minPlayer.y = bottomLeft.y + buffer;
+
+	}
 }
