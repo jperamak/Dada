72673ffe704fed7d9db9a88b72941c7d06a4c720
thePoet <riku.erkkila@gmail.com>
2015-11-22 16:41:14 +0200

    Trampoline

---
+++ Assets/Scripts/LevelsCommon/Trampoline.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trampoline : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+		Hero hero = col.gameObject.GetComponent<Hero>();
+		
+		if (hero == null )
+			return;
+
+		Rigidbody2D rb = hero.GetComponent<Rigidbody2D>();
+		rb.velocity = new Vector3(0f, 80f,0f); 
+		
+
+	}
+}
